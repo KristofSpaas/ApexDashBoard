896d4fcd363a8cffc094704801a86611d039d968
Kristof Spaas <kristof.spaas@student.pxl.be>
2016-02-18 15:20:47 +0100

    Fix setPassword alert

--- controllers/setPasswordController.js
+++ controllers/setPasswordController.js
@@ -10,6 +10,8 @@
     function setPasswordController(apexFactory, $location) {
         var vm = this;
 
+        vm.showAlert = false;
+
         vm.setPassword = function (isValid) {
             if (isValid) {
                 var data = 'email=' + vm.email + '&token=' + vm.token + '&newpassword=' + vm.newPassword + '&confirmpassword='
@@ -19,7 +21,22 @@
                     .success(function () {
                         $location.url('/passwordIsSet');
                     })
-                    .error(function () {
+                    .error(function (err) {
+                        var string = JSON.stringify(err);
+
+                        if (string.indexOf("Confirm") > -1) {
+                            vm.alert = "Het wachtwoord en het bevestig wachtwoord komen niet overeen."
+                        } else if (string.indexOf("Password") > -1) {
+                            vm.alert = "Het wachtwoord moet minstens 6 karakters lang zijn en een hoofdletter en een cijfer bevatten.";
+                        } else if (string.indexOf("email") > -1) {
+                            vm.alert = "Er is geen gebruiker met dit e-mailadres.";
+                        } else {
+                            vm.alert = "Token of wachtwoord is niet juist. Het wachtwoord moet minstens 6 karakters lang zijn en een hoofdletter en een cijfer bevatten."
+                        }
+
+                        alert(string);
+
+                        vm.showAlert = true;
                     });
             }
         };
